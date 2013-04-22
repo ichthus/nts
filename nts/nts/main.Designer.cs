759140d48de6e4e0d6f720be1d4a4bbf9b2f9662
Gregg Reece <gr2m@cs.mtsu.edu>
2013-04-21 20:58:20 -0500

    slight edit

--- nts/nts/main.Designer.cs
+++ nts/nts/main.Designer.cs
@@ -30,12 +30,12 @@
     {
       this.SuspendLayout();
       // 
-      // Form1
+      // main
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(284, 262);
-      this.Name = "Form1";
+      this.ClientSize = new System.Drawing.Size(615, 412);
+      this.Name = "main";
       this.Text = "BlingBling";
       this.Load += new System.EventHandler(this.Form1_Load);
       this.ResumeLayout(false);
